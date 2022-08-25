a46f199445ea4e7312fabf06ead91e263d15bfd3
Sarah Choukah <21149759+schoukah@users.noreply.github.com>
2022-08-25 14:40:04 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,157 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bib-1",
+      "name": "Bib-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.8417328371041233,
+        "pitch": 0.12483681598135377,
+        "fov": 1.3400073842682496
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5862772473990976,
+          "pitch": 0.20187239815990488,
+          "rotation": 23.561944901923464,
+          "target": "1-bib-2-bureau"
+        },
+        {
+          "yaw": 2.046116438482736,
+          "pitch": 0.2802218060065318,
+          "rotation": 0.7853981633974483,
+          "target": "2-bib-3-imprimante"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bib-2-bureau",
+      "name": "Bib-2-bureau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.18067227432705835,
+        "pitch": 0.12678294997180828,
+        "fov": 1.353923198155676
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8934610583284766,
+          "pitch": 0.11117216059244583,
+          "rotation": 5.497787143782138,
+          "target": "2-bib-3-imprimante"
+        },
+        {
+          "yaw": 0.33348834709916275,
+          "pitch": 0.07303646587143042,
+          "rotation": 5.497787143782138,
+          "target": "0-bib-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.901770782335502,
+          "pitch": 0.5989165559800682,
+          "title": "Indice!",
+          "text": "Une nouvelle famille, loin de la famille (Proverbe ivoirien)"
+        }
+      ]
+    },
+    {
+      "id": "2-bib-3-imprimante",
+      "name": "bib-3-imprimante",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.8071583438111798,
+        "pitch": 0.36528889500268846,
+        "fov": 1.353923198155676
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9952315292400229,
+          "pitch": 0.039662639901298036,
+          "rotation": 1.5707963267948966,
+          "target": "1-bib-2-bureau"
+        },
+        {
+          "yaw": 2.3155924173211986,
+          "pitch": 0.4906488274547929,
+          "rotation": 6.283185307179586,
+          "target": "0-bib-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.058428722617572,
+          "pitch": 0.284461183147819,
+          "title": "Indice!",
+          "text": "Un pour tous, tous pour un ! (Alexandre Dumas)"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
