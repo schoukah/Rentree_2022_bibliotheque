5b3d11c8f2b79531c4ddbff283f575996a0ed8b0
Sarah Choukah <21149759+schoukah@users.noreply.github.com>
2022-08-26 21:41:53 -0400

    mis leurre à jour

--- data.js
+++ data.js
@@ -101,8 +101,8 @@ var APP_DATA = {
           "text": "Une nouvelle famille, loin de la famille <a href=\"https://rentree-2022-asso-etudiante.netlify.app/\">(Proverbe ivoirien)</a>"
         }, 
         {
-          "yaw": 1.7317052427718265,
-          "pitch": 0.19165555238625132,
+          "yaw": 1.7322340099812914,
+          "pitch": 0.16312198276660617,
           "title": "Indice!",
           "text": "😝"
         }
